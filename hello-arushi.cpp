7253ff7624587630a4be443ab51f6006404b9f57
Arushi Roy <royaarushi19@yahoo.com>
2022-04-16 16:24:57 +0530

    two test code files added

---
+++ hello-arushi.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    cout << "Enter your name";
+    string name;
+    cin >> name;
+    cout << "Hello " << name << ", how are you?";
+    return 0;
+}
